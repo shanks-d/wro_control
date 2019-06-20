9f0e612ba9943a979fec4d5e10e9899895d5b270
Awadhut <awadhutthube@gmail.com>
2019-06-20 21:36:19 +0530

    Added position controller for all joints of the robot arm

--- scripts/joint_control.py
+++ scripts/joint_control.py
@@ -5,14 +5,24 @@ from std_msgs.msg import Float64
 import math
  
 def talker():
-    pub = rospy.Publisher('/youbot/joint1_position_controller/command', Float64, queue_size=10)
+    pub_0 = rospy.Publisher('/youbot/joint0_position_controller/command', Float64, queue_size=10)
+    pub_1 = rospy.Publisher('/youbot/joint1_position_controller/command', Float64, queue_size=10)
+    pub_2 = rospy.Publisher('/youbot/joint2_position_controller/command', Float64, queue_size=10)
+    pub_3 = rospy.Publisher('/youbot/joint3_position_controller/command', Float64, queue_size=10)
+    pub_4 = rospy.Publisher('/youbot/joint4_position_controller/command', Float64, queue_size=10)
+    pub_5 = rospy.Publisher('/youbot/joint5_position_controller/command', Float64, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
         position = math.pi/6
         rospy.loginfo(position)
-        pub.publish(position)
+        pub_0.publish(position)
+        pub_1.publish(position)
+        pub_2.publish(position)
+        pub_3.publish(position)
+        pub_4.publish(position)
+        pub_5.publish(position)
         rate.sleep()
  
 if __name__ == '__main__':
